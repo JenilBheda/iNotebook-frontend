9d3e52c61fb08355818e6f6bf2c3a5a9a9ebc076
Jenil <jenilbheda@outlook.com>
2022-06-17 00:39:50 +0530

    test 1

--- src/component/Login.js
+++ src/component/Login.js
@@ -7,7 +7,7 @@ const Login = (props) => {
 
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://inotebook-first-project.herokuapp.com/login", {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
       headers: {
         'Content-Type': 'application/json',
